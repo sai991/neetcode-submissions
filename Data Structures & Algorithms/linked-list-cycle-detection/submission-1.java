8c53570d6b5ec7a0f1a1b85a5ea636429e85e4dc
Sai Prathyusha K <saiprathyusha.kanisetti@gwu.edu>
2026-05-29 23:19:12 -0400

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        while(head!=null){
+            if(head.val==1001){
+                return true;
+            }
+            head.val=1001;
+            head=head.next;
+        }
+        return false;  
+    }
+}
